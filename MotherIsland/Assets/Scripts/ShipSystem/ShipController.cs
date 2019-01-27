de8524e0b6a799a754eab69b52be573366e8ddb0
Anas Iqbal <anasiqbal@outlook.com>
2019-01-27 15:35:16 +0500

    Added difficulty
    - Added 3 more ships
    - Added difficulty settings
    - Added character cheer animation

--- MotherIsland/Assets/Scripts/ShipSystem/ShipController.cs
+++ MotherIsland/Assets/Scripts/ShipSystem/ShipController.cs
@@ -18,16 +18,59 @@ public class ShipController : MonoBehaviour
 	public Transform target;
 
 	public Transform shipParent;
-	public Ship shipPrefab;
-
-	public float shipSpawnDelay = 1;
+	public Ship[] shipPrefab;
 
 	[Header("Waypoints Info")]
 	public int maxPointSelectionIndex;
 	public List<WaypointsInfo> spawnPoints;
-	
 
-	public event System.Action OnShipDestroyed;
+	public static event System.Action OnShipDestroyed;
+
+	[Header("Difficulty")]
+	int difficultyIndex = 0;
+	float ShipSpawnDelay {
+		get
+		{
+			return maxShipSpawnDelay - ((maxShipSpawnDelay - minShipSpawnDelay) * (difficultyIndex / 100f));
+		}
+	}
+	public float minShipSpawnDelay = 2;
+	public float maxShipSpawnDelay = 4;
+	int StartShipSelectionIndex {
+		get
+		{
+			if (difficultyIndex >= 20)
+			{
+				return 1;
+			}
+			else if (difficultyIndex >= 40)
+			{
+				return 2;
+			}
+			else return 0;
+			//return 1;
+		}
+	}
+	int EndShipSelectionIndex {
+		get
+		{
+			if (difficultyIndex >= 10)
+			{
+				return 1;
+			}
+			else if (difficultyIndex >= 30)
+			{
+				return 2;
+			}
+			else if (difficultyIndex >= 50)
+			{
+				return 3;
+			}
+			else return 0;
+
+			//return 3;
+		}
+	}
 
 	List<WaypointsInfo> shuffledSpawnPoints;
 	private List<Ship> spawnedShips = new List<Ship>();
@@ -36,8 +79,6 @@ public class ShipController : MonoBehaviour
 	WaypointsInfo pointInfo;
 
 	Vector3 targetHitPosition;
-
-	
 
 	private void OnDisable()
 	{
@@ -48,6 +89,9 @@ public class ShipController : MonoBehaviour
 	{
 		isActive = true;
 		targetHitPosition = target.position + (Vector3.up * 2);
+
+		difficultyIndex = 0;
+
 		StartCoroutine (SpawnShip ());
 	}
 
@@ -55,9 +99,8 @@ public class ShipController : MonoBehaviour
 	{
 		shuffledSpawnPoints = spawnPoints;
 		Initialize ();
-		Debug.LogError("AI Started");
 	}
-	
+
 	public void RemoveOnScreenShips()
 	{
 		spawnedShips.Clear();
@@ -79,14 +122,16 @@ public class ShipController : MonoBehaviour
 			int waypointIndex = Random.Range (0, maxPointSelectionIndex);
 			pointInfo = shuffledSpawnPoints [waypointIndex];
 
-			Ship spawnedShip = Instantiate (shipPrefab, pointInfo.StartingPoint.position, Quaternion.identity, shipParent);
+			int shipIndex = Random.Range(StartShipSelectionIndex, EndShipSelectionIndex + 1);
+
+			Ship spawnedShip = Instantiate (shipPrefab[shipIndex], pointInfo.StartingPoint.position, Quaternion.identity, shipParent);
 			spawnedShip.Initialize (pointInfo, targetHitPosition);
 			spawnedShip.OnDeath += SpawnedShip_OnDeath;
 
 			shuffledSpawnPoints.RemoveAt (waypointIndex);
 			shuffledSpawnPoints.Add (pointInfo);
 
-			yield return new WaitForSeconds (shipSpawnDelay);
+			yield return new WaitForSeconds (ShipSpawnDelay);
 		}
 	}
 
@@ -94,5 +139,7 @@ public class ShipController : MonoBehaviour
 	{
 		if (OnShipDestroyed != null)
 			OnShipDestroyed ();
+
+		difficultyIndex = (difficultyIndex + 1) % 100;
 	}
 }
---
+++ MotherIsland/Assets/Scripts/Utils/CharacterAnimator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+using DG.Tweening;
+
+public class CharacterAnimator : MonoBehaviour {
+
+	public float cheerDuration;
+	public List<Transform> characters;
+
+	private void OnEnable()
+	{
+		ShipController.OnShipDestroyed += ShipController_OnShipDestroyed;
+	}
+
+	private void ShipController_OnShipDestroyed()
+	{
+		for (int i = 0; i < characters.Count; i++)
+		{
+			characters[i].DOPunchPosition(Vector3.up * 0.5f, cheerDuration / 3f).SetLoops(3, LoopType.Yoyo).SetDelay(i * 0.1f).Play();
+		}
+	}
+
+	private void OnDisable()
+	{
+		ShipController.OnShipDestroyed += ShipController_OnShipDestroyed;
+	}
+}
